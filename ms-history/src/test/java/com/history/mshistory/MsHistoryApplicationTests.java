00d3c6f378e3b4daa54c07b48ea807e276b5fd6e
luizaonorio <luiza.onorio@hotmail.com>
2023-09-05 13:58:20 -0300

    feat: create some configurations

---
+++ ms-history/src/test/java/com/history/mshistory/MsHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.history.mshistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHistoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ms-races/src/test/java/com/races/msraces/MsRacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.races.msraces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
