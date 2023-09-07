5c3c2782ab65fa81fdfa3de2e3491a5f58334611
luizaonorio <luiza.onorio@hotmail.com>
2023-09-06 21:52:13 -0300

    feat(ms-cars): Adding CRUD and validation of already registered car and driver [#1]

--- ms-cars/src/main/java/com/cars/mscars/Controller/CarsController.java
+++ ms-cars/src/main/java/com/cars/mscars/Controller/CarsController.java
@@ -1,2 +1,45 @@
-package com.cars.mscars.Controller;public class CarsController {
+package com.cars.mscars.Controller;
+
+import com.cars.mscars.DTO.CarsDTORequest;
+import com.cars.mscars.DTO.CarsDTOResponse;
+import com.cars.mscars.Service.CarsService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/v1/cars")
+public class CarsController {
+
+    @Autowired
+    private CarsService carsService;
+
+    @GetMapping(value = "/get/{id}")
+    public ResponseEntity<CarsDTOResponse> findById(@PathVariable String id) {
+        return new ResponseEntity<>(carsService.findCarById(id), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/get/cars")
+    public ResponseEntity<List<CarsDTOResponse>> findAll() {
+        return new ResponseEntity<>(carsService.findAllCars(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/post")
+    public ResponseEntity<CarsDTOResponse> create(@Valid @RequestBody CarsDTORequest carsDTORequest) {
+        return new ResponseEntity<>(carsService.createCar(carsDTORequest), HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/update/{id}")
+    public ResponseEntity<CarsDTOResponse> update(@Valid @RequestBody CarsDTORequest carsDTORequest, @PathVariable String id) {
+        return new ResponseEntity<>(carsService.updateCar(carsDTORequest, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity deleteById(@PathVariable String id) {
+        return new ResponseEntity<>(carsService.deleteCarById(id), HttpStatus.OK);
+    }
 }
--- ms-cars/src/main/java/com/cars/mscars/Service/CarsService.java
+++ ms-cars/src/main/java/com/cars/mscars/Service/CarsService.java
@@ -1,2 +1,68 @@
-package com.cars.mscars;public class CarsService {
+package com.cars.mscars.Service;
+
+import com.cars.mscars.DTO.CarsDTORequest;
+import com.cars.mscars.DTO.CarsDTOResponse;
+import com.cars.mscars.Entity.Cars;
+import com.cars.mscars.Repositoty.CarsRepository;
+import com.cars.mscars.Service.Exceptions.CarNotFoundException;
+import com.cars.mscars.Service.Validation.ValidCar;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarsService {
+
+    @Autowired
+    private CarsRepository carsRepository;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    public CarsDTOResponse findCarById(String id) {
+        Cars carEntity = carsRepository.findById(id).orElseThrow(() -> new CarNotFoundException(id));
+        CarsDTOResponse carResponse = mapper.map(carEntity, CarsDTOResponse.class);
+        return carResponse;
+    }
+
+    public List<CarsDTOResponse> findAllCars() {
+        List<Cars> carsListEntity = getCarsListOnDB();
+        List<CarsDTOResponse> carslistResponse = carsListEntity.stream().map(car -> mapper.map(car, CarsDTOResponse.class)).toList();
+        return carslistResponse;
+    }
+
+    public CarsDTOResponse createCar(CarsDTORequest carsDTORequest) {
+        ValidCar.validateEqualsCarsAndPilot(getCarsListOnDB(), carsDTORequest);
+        Cars carsConversion = mapper.map(carsDTORequest, Cars.class);
+        Cars carSaveEntity = carsRepository.save(carsConversion);
+        CarsDTOResponse carResponse = mapper.map(carSaveEntity, CarsDTOResponse.class);
+        return carResponse;
+    }
+
+    public CarsDTOResponse updateCar(CarsDTORequest carsDTORequest, String id) {
+        Cars cars = carsRepository.findById(id).orElseThrow(() -> new CarNotFoundException(id));
+        ValidCar.validateEqualsCarsAndPilot(getCarsListOnDB(), carsDTORequest);
+        cars.setBrand(carsDTORequest.getBrand());
+        cars.setModel(carsDTORequest.getModel());
+        cars.setYear(carsDTORequest.getYear());
+        cars.setPilot(cars.getPilot());
+
+        carsRepository.save(cars);
+        CarsDTOResponse carResponse = mapper.map(cars, CarsDTOResponse.class);
+        return carResponse;
+    }
+
+    public String deleteCarById(String id) {
+        carsRepository.findById(id).orElseThrow(() -> new CarNotFoundException(id));
+        carsRepository.deleteById(id);
+        String carDeletedResponse = "Car deleted! With id: " + id;
+        return carDeletedResponse;
+    }
+
+    public List<Cars> getCarsListOnDB() {
+        List<Cars> carsListEntity = carsRepository.findAll();
+        return carsListEntity;
+    }
 }
