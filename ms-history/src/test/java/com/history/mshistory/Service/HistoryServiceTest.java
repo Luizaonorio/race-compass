7522ef6d7d79017f7f60eefbddb303f530f5b343
luizaonorio <luiza.onorio@hotmail.com>
2023-09-13 01:08:49 -0300

    test(ms-history): Add required percentage

---
+++ ms-history/src/test/java/com/history/mshistory/Service/HistoryServiceTest.java
@@ -0,0 +1,21 @@
+package com.history.mshistory.History.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HistoryServiceTest {
+
+    @Test
+    void findHistoryById() {
+        
+    }
+
+    @Test
+    void findAllHistories() {
+    }
+
+    @Test
+    void saveRaceResultOnDB() {
+    }
+}
